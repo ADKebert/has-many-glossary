0eebae50ffe5d46b30d27d84294c29c0ce3d6246
Alan Kebert <adkebert@yahoo.com>
2016-08-23 22:23:21 -0400

    added validation to term and category

--- app/models/term.rb
+++ app/models/term.rb
@@ -3,6 +3,8 @@ class Term < ApplicationRecord
   has_many :categories, through: :category_term_memberships
   belongs_to :user, optional: true
 
+  validates :name, presence: true, uniqueness: true
+
   def owned_by_user?(asking_user)
     user == asking_user
   end
